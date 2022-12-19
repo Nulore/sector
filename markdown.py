3fa0855ef781c9ea6d436b5ae2720097045479c1
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-12-19 16:40:29 +0800

    more debugging

--- markdown.py
+++ markdown.py
@@ -25,7 +25,7 @@ def main():
         
         if not os.path.exists(new_path):
             open(new_path, "x")
-            log(f"created new file {bank_key}.md")
+            log(f"created new file at path: {new_path}")
 
         with open(new_path, "w") as file:
             file.write(f"# {bank_key.capitalize()}s\n")
